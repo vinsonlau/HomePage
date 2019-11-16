38fcda556118f0cd2fcc5fb6b285e3d14cf8ccd8
vinsonlau <vinsonlwp-wm17@student.tarc.edu.my>
2019-11-16 15:40:08 +0800

    Initial

--- app/src/main/java/com/example/android/homepage/ui/news_and_event/NewsAndEventViewModel.kt
+++ app/src/main/java/com/example/android/homepage/ui/news_and_event/NewsAndEventViewModel.kt
@@ -7,7 +7,8 @@ import androidx.lifecycle.ViewModel
 class NewsAndEventViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is news and event Fragment"
+
+
     }
     val text: LiveData<String> = _text
 }
